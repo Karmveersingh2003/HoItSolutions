9509bb3c57a2bc846514dab06dbcfb7268bed7ed
Karamveer Singh <161586290+Karmveersingh2003@users.noreply.github.com>
2026-02-04 18:17:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Homepage/Homepage";
 import About from "./About/About";
 import Contact from "./Contact/Contact";
